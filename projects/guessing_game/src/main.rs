173d1ff04da7215b03289f19be39a1a81ad15c5f
Rebecca Li <rmli@protonmail.ch>
2018-06-11 20:44:29 -0700

    Adding all base files in assignment 1

---
+++ projects/guessing_game/src/main.rs
@@ -0,0 +1,31 @@
+extern crate rand;
+
+use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
+
+
+fn main() {
+    println!("Guess the number!");
+
+    let secret_num = rand::thread_rng().gen_range(1,101);
+    println!("The secret number is: {}", secret_num);
+
+    println!("Input your guess:");
+
+    let mut guess = String::new();
+
+    io:: stdin().read_line(&mut guess)
+      .expect("failed to read line");
+
+    let guess: u32 = guess.trim().parse()
+      .expect("Please type a number");
+
+    println!("you guessed {}", guess);
+
+    match guess.cmp(&secret_num) {
+      Ordering::Less => println!("Too small"),
+      Ordering::Greater => println!("Too big"),
+      Ordering::Equal => println!("ok"),
+    }
+}
